7d255ccf0dcff908ad1a89fbc1c1adcee3c8655d
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-04-24 10:13:41 +0900

    Implement accept the answer as best answer

--- app/Question.php
+++ app/Question.php
@@ -51,5 +51,10 @@ class Question extends Model
         // foreach (Qquestion->answers as $answer )
     }
 
+    public function acceptBestAnswer(Answer $answer)
+    {
+        $this->best_answer_id = $answer->id;
+        $this->save();
+    }
 
 }
