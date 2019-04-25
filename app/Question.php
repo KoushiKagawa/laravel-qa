eae0f46233d9e654cf886e3fbc26a0bef0625d49
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-04-25 13:57:34 +0900

    Implement many to many polymorpic relationship and make the vote question controls working

--- app/Question.php
+++ app/Question.php
@@ -76,4 +76,23 @@ class Question extends Model
     {
         return $this->favorites->count();
     }
+
+    public function votes()
+    {
+        return $this->morphToMany(User::class, 'votable');
+    }
+    public function upVotes()
+    {
+        return $this->votes()->wherePivot('vote', 1);
+    }
+    public function downVotes()
+    {
+        return $this->votes()->wherePivot('vote', -1);
+    }
+    
+    //add
+    public function vote()
+    {
+        return $this->morphToMany(User::class, 'votable');
+    }
 }
