a3638d710e6d51c9ae81d74136969001fd2c6350
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-05-07 16:38:54 +0900

    - Display all questions in root route (front page) - sort the answers by votes - hide the answer's container where no answer - show an alert when question record empty

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,6 +26,9 @@ class RouteServiceProvider extends ServiceProvider
     {
 
         Route::bind('slug', function($slug) {
+            // return  Question::with(['answers.user' , 'answers' => function ($query) {
+            //     $query->orderBy('votes_count' , 'DESC')；
+            // } ])->where('slug', $slug)->first() ?? abort(404);
             return  Question::with('answers.user')->where('slug', $slug)->first() ?? abort(404);
         });
 
