a3b255c07bf0c8ea039d83891e83bf6a2fb9aed5
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-05-17 10:44:20 +0900

    Create New Answer Component

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -36,8 +36,6 @@
             </div>
         </div>
     </div>
-    <answers :question="{{ $question }}"></answers>
-    
-    @include ('answers._create')
+    <answers :question="{{ $question }}"></answers>    
 </div>
 @endsection
