0c1f4ba04dd0a387b100e7ade48a32930f92cd84
KoushiKagawa <koushi.kagawa@resinc.co.jp>
2019-05-23 16:38:09 +0900

    Fix syntax higlight issue when new answer added

--- resources/js/mixins/highlight.js
+++ resources/js/mixins/highlight.js
@@ -2,8 +2,13 @@ import Pris from 'prismjs';
 
 export default {
     methods: {
-        highlight () {
-            const el = this.$refs.bodyHtml;
+        highlight (id = "") {
+            let el;
+            if (!id) {
+                el = this.$refs.bodyHtml;
+            } else {
+                el = document.getElementById(id);
+            }
             console.log('el', el);
             if (el) Prism.highlightAllUnder(el);
         }
